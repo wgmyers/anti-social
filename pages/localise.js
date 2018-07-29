37f8232c5216f7f769fb0d18fbc1513a4c4fa934
Wayne Myers <wgmyers@gmail.com>
2018-07-29 05:11:37 +0100

    i18n partially implemented; blocked.html now calls localise.js

---
+++ pages/localise.js
@@ -0,0 +1,34 @@
+// Modified from code found on
+// https://stackoverflow.com/questions/25467009/internationalization-of-html-pages-for-my-google-chrome-extension
+
+function replace_i18n(obj, tag) {
+    var msg = tag.replace(/__MSG_(\w+)__/g, function(match, v1) {
+        return v1 ? browser.i18n.getMessage(v1) : '';
+    });
+
+    if(msg != tag) obj.innerHTML = msg;
+}
+
+function localiseHtmlPage() {
+    // Localize using __MSG_***__ data tags
+    var data = document.querySelectorAll('[data-localise]');
+
+    for (var i in data) if (data.hasOwnProperty(i)) {
+        var obj = data[i];
+        var tag = obj.getAttribute('data-localise').toString();
+
+        replace_i18n(obj, tag);
+    }
+
+    // Localise everything else by replacing all __MSG_***__ tags
+    var page = document.getElementsByTagName('html');
+
+    for (var j = 0; j < page.length; j++) {
+        var obj = page[j];
+        var tag = obj.innerHTML.toString();
+
+        replace_i18n(obj, tag);
+    }
+}
+
+localiseHtmlPage();
